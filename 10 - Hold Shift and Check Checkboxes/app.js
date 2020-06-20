239811d77efea8f6c921830430e59b161f17bc09
Zheng Zikang <zhengzikangzzz@gmail.com>
2020-06-20 21:12:17 +0800

    add comments

--- 10 - Hold Shift and Check Checkboxes/app.js
+++ 10 - Hold Shift and Check Checkboxes/app.js
@@ -4,20 +4,30 @@ let lastChecked = null;
 
 function handleCheck(e) {
   let inBetween = false;
+  //for shift check
   if (e.shiftKey && this.checked) {
     checkboxes.forEach((checkbox) => {
       console.log(checkbox);
+      //if the checkbox which is checked is the first one or last one
+      //then toggle the inbetween
+      //basiclly "this" is the last checkbox
+      //and lastChecked is "lasrChecked"
+      //also can be reverse
       if (checkbox === this || checkbox === lastChecked) {
+        //prevent hold shift key directly and click checkbox
+        //the rest of inputs those are after the one you selected will be selected too.
         if (lastChecked != null) {
           inBetween = !inBetween;
         }
       }
+      //if the checkbox is between the first and last box
+      //then check them
       if (inBetween) {
         checkbox.checked = true;
       }
     });
   }
-
+  //for shift uncheck
   if (e.shiftKey && !this.checked) {
     console.log("uncheck shift");
     checkboxes.forEach((checkbox) => {
